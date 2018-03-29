8d8eeb3fd18fd5d87ae769a47eec12ab50c109e1
Rimil Dey <rimildeyjsr@gmail.com>
2018-03-29 11:22:27 +0530

    initial commit, got playground working

---
+++ wwdcGame.playground/Contents.swift
@@ -0,0 +1,52 @@
+import PlaygroundSupport
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode!
+    private var spinnyNode : SKShapeNode!
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    func touchDown(atPoint pos : CGPoint) {
+        
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
+
+// Load the SKScene from 'GameScene.sks'
+let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 640, height: 480))
+if let scene = GameScene(fileNamed: "GameScene") {
+    // Set the scale mode to scale to fit the window
+    scene.scaleMode = .aspectFill
+    
+    // Present the scene
+    sceneView.presentScene(scene)
+}
+
+PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
