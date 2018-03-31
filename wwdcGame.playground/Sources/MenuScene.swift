ad2d0f74c5446659ecf0fe5e17978fe9db79c096
Rimil Dey <rimildeyjsr@gmail.com>
2018-03-31 18:43:12 +0530

    changed bg color and renamed file, added menu label

--- wwdcGame.playground/Sources/MenuScene.swift
+++ wwdcGame.playground/Sources/MenuScene.swift
@@ -7,11 +7,14 @@ public class MenuScene: SKScene {
     
     let titleLabel = SKLabelNode(fontNamed: "Noteworthy-Bold")
     let descriptionLabel = SKLabelNode(fontNamed: "Noteworthy-Bold")
+    let menuLabel = SKLabelNode(fontNamed: "Noteworthy-Bold")
    
     override public func didMove(to view: SKView) {
+        self.scene!.backgroundColor = SKColor.black
+        
         titleLabel.text = "Arbitrary"
         titleLabel.fontSize = 65
-        titleLabel.fontColor = SKColor.green
+        titleLabel.fontColor = SKColor.white
         titleLabel.position = CGPoint(x:frame.midX,y:frame.midY)
         titleLabel.alpha = 0.0
         actq.addNext(sprite: titleLabel,action: SKAction.fadeIn(withDuration: 2.0) )
@@ -20,12 +23,20 @@ public class MenuScene: SKScene {
         
         descriptionLabel.text = "A collection of random things"
         descriptionLabel.fontSize = 40
-        descriptionLabel.fontColor = SKColor.green
+        descriptionLabel.fontColor = SKColor.white
         descriptionLabel.position = CGPoint(x:frame.midX,y:frame.midY)
         descriptionLabel.alpha = 0.0
         actq.addNext(sprite: descriptionLabel,action: SKAction.fadeIn(withDuration: 2.0) )
         actq.addNext(sprite: descriptionLabel,action: SKAction.fadeOut(withDuration: 2.0) )
         addChild(descriptionLabel)
+        
+        menuLabel.text = "Menu"
+        menuLabel.fontSize = 40
+        menuLabel.fontColor = SKColor.white
+        menuLabel.position = CGPoint(x:frame.midX,y:frame.midY-100)
+        menuLabel.alpha = 0.0
+        actq.addNext(sprite: menuLabel,action: SKAction.fadeIn(withDuration: 2.0) )
+        addChild(menuLabel)
         
     }
     
