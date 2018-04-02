c69b02dc080e248e65ab3a291c8fffccff0f5e5b
Rimil Dey <rimildeyjsr@gmail.com>
2018-04-02 17:18:57 +0530

    phyllotaxis color

--- wwdcGame.playground/Sources/Phyllotaxis.swift
+++ wwdcGame.playground/Sources/Phyllotaxis.swift
@@ -16,18 +16,37 @@ public class Phyllotaxis : SKScene {
         self.scene!.backgroundColor = SKColor.black
         
         while ( n != 1000) {
-            var rotateAngle = Float(n) * angle
-            var radius = scalingFactor * sqrt(n)
-            var x = Float(radius) * cosf(rotateAngle)
-            var y = Float(radius) * sinf(rotateAngle)
+            let rotateAngle = Float(n) * angle
+            let radius = scalingFactor * sqrt(n)
+            let x = Float(radius) * cosf(rotateAngle)
+            let y = Float(radius) * sinf(rotateAngle)
             
-            let dot = SKShapeNode(circleOfRadius: 5.0)
+            
+            let dot = SKShapeNode(circleOfRadius: 4.0)
             dot.position = CGPoint(x: CGFloat(x) + initialPoint.x , y: CGFloat(y) + initialPoint.y)
-            dot.fillColor = .green
+            let hueValue = decideColor(n)
+            dot.fillColor = SKColor(hue: CGFloat(hueValue),saturation: 1.0,brightness: 1.0,alpha: 1.0)
             dot.zPosition = 20
             addChild(dot)
             
             n += 1
         }
     }
+    
+    func decideColor(_ : Double) -> Float {
+        let rem = n.truncatingRemainder(dividingBy : 10)
+        switch(rem) {
+        	case 1: return 0.1
+            case 2: return 0.2
+            case 3: return 0.3
+            case 4: return 0.4
+            case 5: return 0.5
+            case 6: return 0.6
+            case 7: return 0.7
+            case 8: return 0.8
+            case 9: return 0.9
+        default: return 0.0
+        }
+    }
+    
 }
